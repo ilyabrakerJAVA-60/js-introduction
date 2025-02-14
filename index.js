71fdb2e05a60e2922da1328f789972f306b2ada4
ILIA <brakerilya@yandex.ru>
2025-02-14 20:48:47 +0300

    1 lesson

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello world")
+ const array =["abc",23];
+//  array[1000000]=10;
+ console.log(array.length)
+ console.log(array[0])
+ function forEach(array,fun){
+    for (let i =0;i<array.length;i++){
+        fun(array[i],i);
+    }
+ }
+ function print(element,index){
+    console.log(`index: ${index}, element: ${element}`)
+ }
+ forEach(array,print)
