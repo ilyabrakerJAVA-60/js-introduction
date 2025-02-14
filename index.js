0f16304bebe8d22d27a39e4990cf3c605a53bc9f
ILIA <brakerilya@yandex.ru>
2025-02-14 20:03:00 +0300

    functions as an object

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello world")
+// function is an object with operator  () (function call)
+//conversion function to string will return string containing text of the function
+//conversion function to number will return NaN
+const sum =function(a,b){
+    return a+b
+}
+// console.log(sum+"")
+const subtract =function(a,b){
+    return a-b
+}
+// function calculate(fun,a,b){
+//     return fun(a,b)
+// }
+// console.log(calculate(subtract,10,20))
