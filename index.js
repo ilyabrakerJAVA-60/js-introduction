bd9fe76a0acc0b0254930a0ab45da252590dd75b
ILIA <brakerilya@yandex.ru>
2025-02-16 15:37:06 +0300

    hw update

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-let array = [2,3,4];
+let array = [3,4];
 
 function some(array, fun) {
 let res=false;
-   for(let i = 0; i < array.length; i++) {
-      if (fun(array[i],i)){
-         res =true
+for(let i = 0; i < array.length || res==false; i++) {
+    if (fun(array[i],i)){
+        res =true
       }
       }
 return res
 }
 
 function evenNumber(num) {
-   return num % 2 == 0;
+if(typeof(num)=="number")
+    return num % 2==0;
 }
 
 function every(array, fun) {
 let res=true;
 let flag =0;
-for(let i = 0; i < array.length; i++) {
-   if (!fun(array[i],i)){
-      flag++;
+for(let i = 0; i < array.length || flag<0; i++) {
+    if (!fun(array[i],i)){
+        flag++;
    }
    }
-   if(flag>0){
-      res=false;
+if(flag>0){
+    res=false;
    }
- return res;
+return res;
 }
 
 function elmGreaterIndex(elem, index) {
-   return elem > index
+if(typeof(elem)=="number")
+    return elem > index
 }
+
 console.log(`using "every" function for even numbers array: ${array}, function ${evenNumber}, result: ${every(array, evenNumber)} - false`)
 console.log(`using "some" function for even numbers array: ${array}, function ${evenNumber}, result: ${some(array, evenNumber)} - true`)
 console.log(`using "every" function for elements greater than the index values, array: ${array}, function ${elmGreaterIndex}, result: ${every(array, elmGreaterIndex)} - true`)
