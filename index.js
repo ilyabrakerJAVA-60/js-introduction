fd32f147d40c756965b431f3abfc6ea5698b7f82
ILIA <brakerilya@yandex.ru>
2025-02-16 21:34:52 +0300

    variadic parametr/spread opeator

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-// function is an object with operator  () (function call)
-//conversion function to string will return string containing text of the function
-//conversion function to number will return NaN
-const sum =function(a,b){
-    return a+b
+function sum(...operands){ // сколько угодно аргументов
+    let sum=0;
+    // if(Array.isArray(operands[0]))
+    //     operands=[...operands[0]]
+    for(let i=0; i < operands.length;i++){
+        if(Array.isArray(operands[i])){
+            operands=[...operands[i]]
+        }
+    sum=sum+operands[i]; 
+       
+    }
+    return sum
 }
-// console.log(sum+"")
-const subtract =function(a,b){
-    return a-b
-}
-// function calculate(fun,a,b){
-//     return fun(a,b)
-// }
-// console.log(calculate(subtract,10,20))
+console.log(`sum(1,2,3)=${sum(1,2,"hello")}`)
+console.log(`sum([1,2,3])=${sum([1,2,3],4,5,6)}`)
+console.log(Math.max(1,2,3))
+console.log(Math.max([1,2,3]))
