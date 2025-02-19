f4dfbb8552fc89e75c55bb3503a6412ce7721fae
ILIA <brakerilya@yandex.ru>
2025-02-19 16:03:23 +0300

    1 variant HW

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-function sum(...operands){ // сколько угодно аргументов
-    let sum=0;
-    // if(Array.isArray(operands[0]))
-    //     operands=[...operands[0]]
-    for(let i=0; i < operands.length;i++){
-        if(Array.isArray(operands[i])){
-            operands=[...operands[i]]
+function minMax(...operands){
+    let array_new=[];
+    let index=0;
+    let result=[Infinity,-Infinity]
+    for (let i=0;i<operands.length;i++){
+        if (Array.isArray(operands[i])){
+            ifHaveisArray(operands[i],array_new,index)   
         }
-    sum=sum+operands[i]; 
-       
+        else{
+            array_new[index]=operands[i]
+            index++;
+        }
     }
-    return sum
+    result=find_minMax(array_new,result)
+    return result
 }
-console.log(`sum(1,2,3)=${sum(1,2,"hello")}`)
-console.log(`sum([1,2,3])=${sum([1,2,3],4,5,6)}`)
-console.log(Math.max(1,2,3))
-console.log(Math.max([1,2,3]))
+function find_minMax(array_new,result){
+    let newmin=result[0];
+    let newmax=result[1];
+    for(let i=0; i<array_new.length;i++){
+        if(newmin>array_new[i])
+            newmin=array_new[i]
+        if (newmax<array_new[i])
+            newmax=array_new[i]
+    }
+    return [newmin,newmax]
+
+}
+
+function ifHaveisArray(operands,new_Array,index){
+    for(let j=0; j<operands.length;j++){
+        new_Array[index]=operands[j];
+        index++;
+    }   
+    }
+console.log(minMax(1,2,[3,4,6],5,7,9,[1,2,12],-1))
+
+
+
