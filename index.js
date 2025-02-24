9d63ee4bb6422b12baf9d611509f3295d07de565
ILIA <brakerilya@yandex.ru>
2025-02-25 01:22:14 +0300

    arrow function introduction

--- index.js
+++ index.js
@@ -1,41 +1,31 @@
-function minMax(...operands){
-    let result=[Infinity,-Infinity]
-    for (let i=0;i<operands.length;i++){
-        if (Array.isArray(operands[i])){
-            let ArrayMinMax=findMinMaxinArray(operands[i])
-            result=getMinMax(result,ArrayMinMax[0],ArrayMinMax[1])
-        }
-        else
-            result=getMinMax(result,operands[i])
-    
-    }
-    
-    return result
+//arrow functions
+const sumFun=function(op1,op2){
+    return op1+op2; 
 }
-function findMinMaxinArray(array_in_array){
-    let result=[Infinity,-Infinity]
-    let newmin=result[0];
-    let newmax=result[1];
-    for(let i=0; i<array_in_array.length;i++){
-        if(newmin>array_in_array[i])
-            newmin=array_in_array[i]
-        if (newmax<array_in_array[i])
-            newmax=array_in_array[i]
-    }
-    return [newmin,newmax]
-}
-function getMinMax(result,min,max=min){
-    if(result[0]>min){
-        result[0]=min
-    }
-    if(result[1]<max){
-        result[1]=max
-    }
- return result
-}
-array1=[4,1,2,7,8]
-console.log(minMax(array1))
-console.log(minMax(1,2,3))
-console.log(minMax(1,2,3,[100, 50]))
-console.log(minMax(1,2,3,[100,50],[-2,40,200]))
+const sumArrowFun=(op1,op2) => op1+op2;
+//statement
+// let c,a=10,b=3
+// if (a >b){
+//     c=a+b;
+// } else{
+//     c=a*b;
+// }
+// c = a > b ? a + b : a * b
+// const printC =function(){
+//   let c,a=10,b=3
+// if (a > b){
+//     c=a+b;
+// } else{
+//     c=a*b;
+// }  
+// console.log(c)
+// }
 
+// printC()
+// let a = 10,b = 3
+// const printCarrow=(a,b)=> console.log(a>b? a+b : a*b)
+// printCarrow()
+// let array=[10,-7,120]
+// const sorted1=array.sort((a,b)=> a>b ? 1:-1 ) 
+// console.log(sorted1)
+console.log([10,-7,120].sort((a,b)=>a-b))
