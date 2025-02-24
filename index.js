800011e0b5bf61182523c3cc5784fd79c52c6b3d
ILIA <brakerilya@yandex.ru>
2025-02-24 17:15:13 +0300

    methods

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
-const array=['HELLO',122,-10,'Java','JavaScript',500,'Nodejs']
-//добавить в массив
-// array.push(...[1,2,3])//добавляет аргументы в массив
-// console.log(array)
-// console.log([1,2,3].push(4,5,6))// возвращает кол-во элементов, после добавления,добавляет в конец 
-// // console.log([1,2,3].concat([4,5,6]))
-// const array1=[1,2,3].concat([3,4,5])
-// console.log(array1[4])
-// const ar1=[3,4,5].concat(array)
-// console.log(ar1)
-// console.log(array.unshift(3,4,5)) // возвращает кол-во элементов, добавляет в начало
-// console.log(array)
-// console.log(array.splice(3,0,"kuku"))// добавляет удаляет где угодно
-// console.log(array)
-// console.log(array.pop())// удаляет массив последний и возвращает его
-// console.log(array)
-// console.log(array.shift())// удаляет массив первый и возвращает его
-// console.log(array)
-console.log(array.splice(3,1,"python"))
-console.log(array)
+ array=['HELLO',122,-10,'Java','JavaScript',500,'Nodejs']
+// // найти индекс 
+// let index=array.lastIndexOf("Java");
+// console.log(index);
+const array1=array.filter(function(e,index){
+    let res=false;
+    if(typeof e=="string"){
+        res=e.includes("Java")
+        
+    }
+    return (res)
+})
+console.log(array1)
+// array.forEach(function(e,i){
+//     console.log(i+1,e)
+// })
+
+// array.map("Java")
+
+
+let numbers = [1, 2, 3, 4, 5, 6];
+
+// Фильтруем четные числа
+let evenNumbers = numbers.map(function(num) {
+  if(num % 2 === 0)
+    return(`item :${num}`)
+ return null
+}).join("  ");
+
+console.log(evenNumbers); // [2, 4, 6]
+
+const array2 = [122, 10000, 2, 9, 100000000];
+console.log(array2.toSorted());
+console.log(array2)
+const array3 = [122, 10000, 2, 9, 100000000];
+// console.log(array1.sort());
+// console.log(array1)
+// console.log(array1.toSorted());
+// console.log(array1);
+console.log(array3.toSorted(function(a, b){
+    return a - b;
+}))
