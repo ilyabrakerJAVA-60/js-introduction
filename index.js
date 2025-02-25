c5eec95902acdd0bb1d309fd94fadc75b1ebba77
ILIA <brakerilya@yandex.ru>
2025-02-25 16:39:25 +0300

    some/every/map/filter,reduce,forEach

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
- array=['JavaScript',122,-10,'Java','HELLO',500,'Nodejs']
- function compNumStr(e1, e2){
-    let res=-1;
-    if(typeof(e1)=="number"&&typeof(e2)=="number")
-        res=e2-e1
-    else if(typeof(e1)!="number"&&typeof(e2)!="number")
-        res = e1 > e2 ? 1 : (e1 < e2 ? -1 : 0);
-    else if(typeof(e1)!="number")
-        res=1
-    return res
- }
- console.log(array.sort(compNumStr))
- 
+ let array=['JavaScript',122,-10,'Java','HELLO',500,'Nodejs']
+let array1=array.slice()// copy array
+
+ function isNumber(element){
+    return (typeof(element)=="number")
+       
+}
+
+
+
  function orderedList(array) {
  const items=array.map(getItem).join("")
  return `<ol>${items}<ol>`
@@ -22,6 +19,18 @@ function getItem(item){
     return res
 
 }
+array=array.filter(e=>isNumber(e))
+console.log('array.some(a => a<0)=>',array.some(a => a<0))
+console.log('array.every(a => a<0)=>',array.every(a=> a<0))
+console.log(`sum of array [${array}] =>`,array.reduce((acc,cur)=>acc+cur))
+ console.log(`minimal value of array [${array}] =>`, array.reduce((acc,cur)=>(acc<cur?acc:cur),0))
+ array=array1.slice(1,3);
   const bodyElement = document.querySelector("body");
-  bodyElement.innerHTML = orderedList(["Hello", 300, "Java", "Nodejs", 100]);
-
+  bodyElement.innerHTML = orderedList(array);
+//объяснение методов,которые обрабатывают массив с callback
+// forEach-это метод массива, который позволяет выполнить определенную функцию для каждого элемента массива. Он не возвращает новый массив, а просто выполняет побочные действия для каждого элемента.
+//map- используется для преобразования элементов массива в новый массив, основываясь на том, что возвращает функция, переданная в map. Этот метод создает новый массив с результатами вызова функции для каждого элемента исходного массива, не изменяя сам массив.
+//filter-используется для фильтрации элементов массива на основе заданного условия, создавая новый массив, содержащий только те элементы, которые удовлетворяют этому условию. Исходный массив не изменяется.
+//some-используется для проверки, удовлетворяет ли хотя бы один элемент массива заданному условию. Этот метод возвращает true, если хотя бы один элемент массива удовлетворяет условию, и false, если ни один элемент не удовлетворяет условию. 
+// every-проверяет, удовлетворяют ли все элементы массива условию
+//reduce-Это позволяет выполнить такие операции, как суммирование, умножение, создание объектов и многие другие, обрабатывая элементы массива поочередно и аккумулируя результат.
