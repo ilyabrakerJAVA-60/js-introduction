fda347cb4832ee69cd6d8687b15485e061da1f35
ILIA <brakerilya@yandex.ru>
2025-02-26 01:03:46 +0300

    update hw16

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 function myReduce(array, callback, initialValue) {
-    let accumulator = initialValue !== undefined ? initialValue : array[0];
-    let startIndex = initialValue !== undefined ? 0 : 1;
-    for (let i = startIndex; i < array.length; i++) {
-     res = callback(accumulator, array[i], i, array);
-    }
-  
-    return res;
+  let accumulator = initialValue === undefined ? [array[0],array[0]]:initialValue;
+  let startIndex = initialValue !== undefined ? 0 : 1;
+  for (let i = startIndex; i < array.length; i++) {
+      accumulator = callback(accumulator, array[i], i, array); // Перезаписываем аккумулятор новым значением
   }
-function MyminMax(array) {
+  return accumulator;
+}
+  function MyminMax(array) {
     return myReduce(array, (accumulator, currentValue) => {
-      accumulator[0] = currentValue < accumulator[0] ? currentValue : accumulator[0]; // минимальное значение
-      accumulator[1] = currentValue > accumulator[1] ? currentValue : accumulator[1]; // максимальное значение
-      return accumulator;
-    }, [array[0], array[0]]);
-  }
+        // Вместо изменения аккумулятора, создаем новый массив с обновленными min и max
+        const newMin = currentValue < accumulator[0] ? currentValue : accumulator[0]; // Находим новый минимум
+        const newMax = currentValue > accumulator[1] ? currentValue : accumulator[1]; // Находим новый максимум
+        return [newMin, newMax]; // Возвращаем новый массив с обновленными min и max
+    });
+}
+
 const array = [3, 1, 4, 1, 5, 9, 2];
-let [min,max]=MyminMax(array)
-console.log(`min of array=> ${min} , max of array=> ${max}`)
+let [min, max] = MyminMax(array);
+console.log(`min of array=> ${min} , max of array=> ${max}`);
